120ce637b4610012d0f98f250444693135757b7c
dimexx87 <62471957+dimexx87@users.noreply.github.com>
2020-10-12 11:29:43 +0300

    add error routing for different types of request

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const { PORT = 3000, BASE_PATH } = process.env;
 const path = require('path');
-const userUserRoutes = require('./routes/users');
+const userRoutes = require('./routes/users');
 const cardsRoutes = require('./routes/cards');
 const errorRoutes = require('./routes/error');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', userUserRoutes);
+app.use('/', userRoutes);
 app.use('/', cardsRoutes);
 app.use('/', errorRoutes);
 
--- routes/error.js
+++ routes/error.js
@@ -2,5 +2,8 @@ const router = require('express').Router();
 const getError = require('../controllers/error');
 
 router.get('*', getError);
+router.post('*', getError);
+router.put('*', getError);
+router.delete('*', getError);
 
 module.exports = router;
